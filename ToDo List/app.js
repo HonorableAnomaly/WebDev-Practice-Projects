d5d1b8fa78cad173edd9ea30dc84c915de4fddb6
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-01 20:32:09 -0800

    Update app.js
    -removed let variable start
    -removed while loop for user input prompt 'new' containing new prompt and for of loop, printing created variable 'added'
    +let variable input with user prompt
    +const variable todos with two elements in the array
    +while loop that checks if the user input was 'quit' or 'q', if the input is 'list' prints stars and runs a for loop that prints the todos array
    +else if statement to detect user input 'new' using a user input prompt saved in const variable 'newTodo, pushed into todos, printing the newly added element
    +else if statement to detect user input 'delete' using a parseInt user input prompt, an if else statement to detect if user input is a number in index, const variable deleted splices todos to remove the user selected index, printing what was removed, and an else statement that prints unknown index
    +input variable reassignment to repeat prompt for user input
    -removed while loop detecting user input 'list' with variable reassignment for user prompt, for of loop that iterates across each character of variable 'start', printing list
    -removed while loop detecting user input 'delete' with reassigned variable 'start' user input prompt
    -removed while loop that detects user input 'quit'
    +printing statement for quitting the app

--- ToDo List/app.js
+++ ToDo List/app.js
@@ -1,21 +1,25 @@
-let start = prompt("What would you like to do?");
-
-while (start.toLowerCase() === "new") {
-  start = prompt("What ToDo would you like to add?");
-  for (added of start) {
-    console.log(added);
+let input = prompt('What would you like to do?');
+const todos = ['Buy handpan', 'Find car title'];
+while (input !== 'quit' && input !== 'q') {
+  if (input === 'list') {
+    console.log('***************');
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log('***************');
+  } else if (input === 'new') {
+      const newTodo = prompt('What todo would you like to add?');
+      todos.push(newTodo);
+      console.log(`${newTodo} has been added to your list!`);
+  } else if (input === 'delete') {
+      const index = parseInt(prompt('What is the index of the ToDo you wish to remove?'));
+        if (!Number.isNaN(index)) {
+        const deleted = todos.splice(index, 1);
+        console.log(`${deleted[0]} was deleted from your list.`);
+        } else {
+          console.log('Unknown index.');
+        }
   }
+  input = prompt('What would you like to do?');
 }
-
-while (start.toLowerCase() === "list") {
-  start = prompt("Here's your list so far. What would you like to do?");
-  for (list of start) {
-    console.log(list);
-  }
-}
-
-while (start.toLowerCase() === "delete") {
-  start = prompt("What is the index of the ToDo you wish to remove?");
-}
-
-while (start.toLowerCase() === "quit") {}
+console.log('The app has been closed.');
